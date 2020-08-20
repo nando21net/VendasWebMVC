a65170b923612f9ca9ed997145cb4fdc594f0fe3
Fernando Dias seixas <nando21.net@gmail.com>
2020-08-20 11:56:42 -0300

    SellerService FindAll síncrono

---
+++ VendasWebMVC/Servicos/VendedoresServico.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VendasWebMVC.Data;
+using VendasWebMVC.Models;
+
+namespace VendasWebMVC.Servicos
+{
+    public class VendedoresServico
+    {
+        private readonly VendasWebMVCContext _context;
+
+        public VendedoresServico(VendasWebMVCContext context)
+        {
+            _context = context;
+
+        }
+
+        public List<Vendedor> FindAll()
+        {
+            return _context.Vendedor.ToList();
+        }
+    }
+}
