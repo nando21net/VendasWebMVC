586fcdacd45bbe7a0e12ffa1871de7540425a00b
Fernando Dias seixas <nando21.net@gmail.com>
2020-09-08 09:04:34 -0300

    Retornando Pagina Erro Personalizada

--- VendasWebMVC/Controllers/VendedoresController.cs
+++ VendasWebMVC/Controllers/VendedoresController.cs
@@ -8,76 +8,76 @@ using VendasWebMVC.Models;
 using VendasWebMVC.Models.ViewModels;
 using VendasWebMVC.Servicos;
 using VendasWebMVC.Servicos.Exceptions;
-//using VendasWebMVC.Servicos.Exceptions;
 
-
-
-namespace VendasWebMVC.Controllers
+namespace SalesWebMvc.Controllers
 {
     public class VendedoresController : Controller
     {
-        private readonly VendedoresServico _vendedoresServico;
-        private readonly DepartamentoServico _departamentoServico;
+        private readonly VendedoresServico _sellerService;
+        private readonly DepartamentoServico _departmentService;
 
-        public VendedoresController(VendedoresServico vendedoresServico, DepartamentoServico departamentoServico)
+        public VendedoresController(VendedoresServico sellerService, DepartamentoServico departmentService)
         {
-            _vendedoresServico = vendedoresServico;
-            _departamentoServico = departamentoServico;
+            _sellerService = sellerService;
+            _departmentService = departmentService;
         }
+
         public IActionResult Index()
         {
-            var list = _vendedoresServico.FindAll();
+            var list = _sellerService.FindAll();
             return View(list);
         }
 
         public IActionResult Create()
         {
-            var departamentos = _departamentoServico.FindAll();
-            var viewModel = new VendedorFormViewModel { Departamentos = departamentos };
+            var departments = _departmentService.FindAll();
+            var viewModel = new VendedorFormViewModel { Departamentos = departments };
             return View(viewModel);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create(Vendedor vendedor)
+        public IActionResult Create(Vendedor seller)
         {
-            _vendedoresServico.Insert(vendedor);
+            _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
 
         public IActionResult Delete(int? id)
-        
-       {
+        {
             if (id == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not provided" });
             }
-            var obj = _vendedoresServico.FindById(id.Value);
+
+            var obj = _sellerService.FindById(id.Value);
             if (obj == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not found" });
             }
 
             return View(obj);
         }
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Delete(int id)
         {
-            _vendedoresServico.Remove(id);
+            _sellerService.Remove(id);
             return RedirectToAction(nameof(Index));
         }
+
         public IActionResult Details(int? id)
         {
             if (id == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not provided" });
             }
 
-            var obj = _vendedoresServico.FindById(id.Value);
+            var obj = _sellerService.FindById(id.Value);
             if (obj == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not found" });
             }
 
             return View(obj);
@@ -87,43 +87,47 @@ namespace VendasWebMVC.Controllers
         {
             if (id == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not provided" });
             }
 
-            var obj = _vendedoresServico.FindById(id.Value);
+            var obj = _sellerService.FindById(id.Value);
             if (obj == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not found" });
             }
 
-            List<Departamento> departments = _departamentoServico.FindAll();
+            List<Departamento> departments = _departmentService.FindAll();
             VendedorFormViewModel viewModel = new VendedorFormViewModel { Vendedor = obj, Departamentos = departments };
             return View(viewModel);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Edit(int id, Vendedor vendedor)
+        public IActionResult Edit(int id, Vendedor seller)
         {
-            if (id != vendedor.Id)
+            if (id != seller.Id)
             {
-                return BadRequest();
+                return RedirectToAction(nameof(Error), new { message = "Id mismatch" });
             }
             try
             {
-                _vendedoresServico.Update(vendedor);
+                _sellerService.Update(seller);
                 return RedirectToAction(nameof(Index));
             }
-            catch (NotFoundException)
+            catch (ApplicationException e)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = e.Message });
             }
-            catch (DbConcurrencyException)
+        }
+
+        public IActionResult Error(string message)
+        {
+            var viewModel = new ErrorViewModel
             {
-                return BadRequest();
-            }
+                Message = message,
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+            };
+            return View(viewModel);
         }
     }
 }
-   
-
